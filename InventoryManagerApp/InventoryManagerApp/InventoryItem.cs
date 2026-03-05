b90db994aed75aba41b74fccbfa93d01b6fab8fa
Alexandr Kuznetcov <232909072092-11>
2026-03-05 11:57:14 +0300

    Добавллен класс InventoryItem

---
+++ InventoryManagerApp/InventoryManagerApp/InventoryItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagerApp
+{
+    internal class InventoryItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public InventoryItem(string name, int quantity, decimal price, string category)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Category = category;
+        }
+    }
+}
